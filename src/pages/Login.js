b6c413a31073edab5044a486ab035b468c352e5f
Brunobmendes <Bruno.mendes2804@gmail.com>
2022-11-09 17:34:14 -0300

    requisito 3: em adamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
   constructor() {
@@ -24,6 +25,11 @@ class Login extends React.Component {
     const emailValidate = email.length >= size;
     const buttonState = nameValidate && emailValidate;
     this.setState({ loginDisabled: !(buttonState) });
+  };
+
+  handleSettingsCLick = () => {
+    const { history } = this.props;
+    history.push('/settings');
   };
 
   render() {
@@ -59,10 +65,23 @@ class Login extends React.Component {
           Play
 
         </button>
-
+        <br />
+        <button
+          type="button"
+          data-testid="btn-settings"
+          onClick={ this.handleSettingsCLick }
+        >
+          Settings
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
---
+++ src/pages/Settings.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <h1
+          data-testid="settings-title"
+        >
+          Configurações
+
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Settings;
