adb0e384c61f4b53ad704f4b1b72d624d8919bd9
Iago Viotti <iago.viotti@gmail.com>
2022-11-09 19:22:14 -0300

    req 5 lint proof

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import { connect } from "react-redux";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
