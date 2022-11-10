55b959471331aefded16651d1d8ece40e02aeca7
Gabriel Galera <gabgalera@hotmail.com>
2022-11-10 15:24:39 -0300

    Requisito 4 completo

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -5,27 +5,42 @@ import App from '../App'
 import userEvent from '@testing-library/user-event';
 
 describe('Testando a tela de login', () => {
-  test('Se o inputs e o botão funcionam', async () => {
+  test('Se o inputs e o botão de play funcionam', async () => {
     const { history } = renderWithRouterAndReducer(<App />)
 
     const inputs = screen.getAllByRole('textbox')
     const name = inputs[0]
     const email = inputs[1]
-    const button = screen.getByRole('button')
+    const buttonPlay = screen.getByTestId('btn-play')
 
-    expect(button).toBeDisabled()
+    expect(buttonPlay).toBeDisabled()
 
     userEvent.type(name, 'Nome de teste');
     userEvent.type(email, 'youPass@gmail.com');
 
-    expect(button).not.toBeDisabled()
+    expect(buttonPlay).not.toBeDisabled()
     
-    userEvent.click(button)
+    userEvent.click(buttonPlay)
     
     const newPage = await screen.findByText(/gameon/i)
     
     expect(newPage).toBeInTheDocument()
     expect(history.location.pathname).toMatch('/game')
+
+  });
+  test('Se o inputs e o botão de Configuração funciona', async () => {
+    const { history } = renderWithRouterAndReducer(<App />)
+
+    const buttonPlay = screen.getByTestId('btn-settings')
+
+    expect(buttonPlay).not.toBeDisabled()
+    
+    userEvent.click(buttonPlay)
+    
+    const newPage = await screen.findByText(/configurações/i)
+    
+    expect(newPage).toBeInTheDocument()
+    expect(history.location.pathname).toMatch('/settings')
 
   })
 })
